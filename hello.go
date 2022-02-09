b72607d79aba7ba1c4092f9ea0cc5af41f27bb93
epthalite <fmy628.1998@icloud.com>
2022-02-09 23:52:36 +0900

    add range method

--- hello.go
+++ hello.go
@@ -1,5 +1,8 @@
 package main
 import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
 func main(){
 	s := []int{2,3,5,7,11,13}
 	fmt.Println(s)
@@ -12,6 +15,12 @@ func main(){
 	// 2 < x <= 3 なので　{7}
 	s = s[2:]
 	fmt.Println(s)
+
+	// forEachと一緒、iは今の番号、vは数字
+		for i, v := range pow {
+		fmt.Printf("--------------\n")
+		fmt.Printf("%d\n",i)
+		fmt.Printf("%d\n",v)
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
-
-
