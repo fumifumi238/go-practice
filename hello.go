7366a17d596728fcdfe1cea50e9a87f51a9dc09d
epthalite <fmy628.1998@icloud.com>
2022-02-10 23:33:25 +0900

    add map

--- hello.go
+++ hello.go
@@ -3,6 +3,14 @@ import "fmt"
 
 var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
+// 型定義
+type Vertex struct {
+	Lat, Long float64
+}
+
+// 宣言　mapでkey,valueの形を定義
+var m map[string]Vertex
+
 func main(){
 	s := []int{2,3,5,7,11,13}
 	fmt.Println(s)
@@ -22,5 +30,22 @@ func main(){
 		fmt.Printf("%d\n",i)
 		fmt.Printf("%d\n",v)
 		fmt.Printf("2**%d = %d\n", i, v)
+	}
+
+fmt.Printf("%v\n",m)
+// 宣言　mapでkey,valueの形の配列を作成
+	m = make(map[string]Vertex)
+	fmt.Printf("%v\n",m)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+
+	m["Mickel Jackson"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+	fmt.Println(m["Mickel Jackson"])
+	for key,value := range m{
+		fmt.Println(key,value)
 	}
 }
